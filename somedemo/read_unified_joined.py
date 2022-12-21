f87b89a1ea4c80b6a7d9fedf432baa64bedad3cc
ume-technology <fanzhimin1993@gmail.com>
2022-12-21 14:22:59 +0800

    others.1

--- somedemo/read_unified_joined.py
+++ somedemo/read_unified_joined.py
@@ -8,20 +8,5 @@
 """
 import pickle
 
-# with open('joinAD_product.pick', 'rb') as f:
-#     joinda = pickle.load(f)
-#
-# with open('joinAD_product_no.pick', 'rb') as f:
-#     joinda_no = pickle.load(f)
-
-# with open('../nowgoodstags/save_mergea_data/unified_joined/0-25000tags.pick', 'rb') as f:
-#     data = pickle.load(f)
-
-# with open('dict_info.dict', 'wb') as f:
-#     pickle.dump(data[5], f)
-
-# with open('../nowgoodstags/save_mergea_data/unified_joined_no/0-50000.pick', 'rb') as f:
-#     data_no = pickle.load(f)
-
-with open('../nowgoodstags/save_mergea_data/unified_joined_new/all.pick','rb') as f:
+with open('../bigfiles/unified_joined_new/all.pick', 'rb') as f:
     data = pickle.load(f)
